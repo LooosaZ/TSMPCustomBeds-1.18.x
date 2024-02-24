7d11c33e07b602e86d26088f01d07735bf18186a
LoosaZ <losartorii@outlook.com>
2024-02-24 14:10:45 +0000

    removed comment

--- src/main/java/net/loosaz/luzamod/items/ModItems.java
+++ src/main/java/net/loosaz/luzamod/items/ModItems.java
@@ -11,11 +11,11 @@ public class ModItems {
         public static final DeferredRegister<Item> ITEMS =
                 DeferredRegister.create(ForgeRegistries.ITEMS, LuzaMod.MOD_ID);
 
-//        public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
-//                () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LUZABOT_TAB)));
-//
-//        public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine",
-//                () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LUZABOT_TAB)));
+        public static final RegistryObject<Item> CITRINE = ITEMS.register("citrine",
+                () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LUZABOT_TAB)));
+
+        public static final RegistryObject<Item> RAW_CITRINE = ITEMS.register("raw_citrine",
+                () -> new Item(new Item.Properties().tab(ModCreativeModeTab.LUZABOT_TAB)));
 
 
         public static void register(IEventBus eventBus) {
