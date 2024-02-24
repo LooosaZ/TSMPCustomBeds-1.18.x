0cea924ce9066ea6a07de314fe7bdac20122194d
LoosaZ <losartorii@outlook.com>
2024-02-24 13:52:44 +0000

    Added items and blocks

--- src/main/java/net/loosaz/luzamod/LuzaMod.java
+++ src/main/java/net/loosaz/luzamod/LuzaMod.java
@@ -3,10 +3,13 @@ package net.loosaz.luzamod;
 import com.mojang.logging.LogUtils;
 import net.loosaz.luzamod.block.ModBlocks;
 import net.loosaz.luzamod.items.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
@@ -24,6 +27,7 @@ public class LuzaMod {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(eventBus);
+
         ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
@@ -32,6 +36,10 @@ public class LuzaMod {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
+    private void clientSetup(final FMLClientSetupEvent event) {
+
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EGG_BED.get(), RenderType.cutout());
+    }
     private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
--- src/main/java/net/loosaz/luzamod/block/ModBlocks.java
+++ src/main/java/net/loosaz/luzamod/block/ModBlocks.java
@@ -1,12 +1,14 @@
 package net.loosaz.luzamod.block;
 
 import net.loosaz.luzamod.LuzaMod;
+import net.loosaz.luzamod.block.custom.EggBed;
 import net.loosaz.luzamod.items.ModCreativeModeTab;
 import net.loosaz.luzamod.items.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -24,6 +26,8 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL)
                     .strength(9f).requiresCorrectToolForDrops()), ModCreativeModeTab.LUZABOT_TAB);
 
+    public static final RegistryObject<Block> EGG_BED = registerBlock("egg_bed",
+            () -> new EggBed(BlockBehaviour.Properties.copy(Blocks.OAK_PLANKS).noOcclusion()), ModCreativeModeTab.LUZABOT_TAB);
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn, tab);
